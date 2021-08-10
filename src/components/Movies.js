502db6f727f868f9b6b5c8e33efc50f1846deea3
grantba <grant.blaire@gmail.com>
2021-08-10 08:55:47 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+        return(
+          <div>
+            <h2 key={movie.title} style={{textDecoration: "underline"}}>{movie.title}</h2>
+            <h2>Runtime: {movie.time}</h2>
+            <h2>Genres:</h2>
+            <ul>
+              {movie.genres.map(genre => {
+                return <li>{genre}</li>})}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
